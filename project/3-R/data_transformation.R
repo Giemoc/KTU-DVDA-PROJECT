b63f16f6ab3bf533ec4801fa95b3e579c3549477
Giemoc <116371400+Giemoc@users.noreply.github.com>
2022-12-20 22:03:44 +0200

    Add files via upload

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,38 @@
 library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
-### save combined file into 1-data directory
+
+install.packages("tidyverse")
+install.packages("dplyr")
+
+library(tidyverse)
+library(dplyr)
+
+data <- read_csv("../1-data/1-sample_data.csv")
+data_additional <- read_csv("../1-data/2-additional_data.csv")
+additional_features <- read_csv("../1-data/3-additional_features.csv")
+
+
+joined_data1 <- merge(data, data_additional, all = TRUE)
+full_data <- inner_join(joined_data1, additional_features, by = "id")
+
+
+#Splitting data into 3 files: 60% train, 20% validation, 20% test
+
+spec = c(train = .6, test = .2, validate = .2)
+
+g = sample(cut(
+  seq(nrow(full_data)), 
+  nrow(full_data)*cumsum(c(0,spec)),
+  labels = names(spec)
+))
+
+res = split(full_data, g)
+
+
+write_csv(res$train, "../1-data/train_data2_3.csv")  
+write_csv(res$validate, "valid_data2_3.csv")
+write_csv(res$test, "test_data2_3.csv")
+
+
+
+
+
--- project/3-R/modelling.R
+++ project/3-R/modelling.R
@@ -1,2 +1,88 @@
+
+install.packages("h2o")
 library(h2o)
 h2o.init()
+
+library(tidyverse)
+library(dplyr)
+
+
+#Training data
+dtrain <- h2o.importFile("../1-data/train_data2.csv")
+dtrain
+class(dtrain)
+summary(dtrain)
+# # 
+y <- "y"
+x <- setdiff(names(dtrain), c(y, "id"))
+dtrain$y <- as.factor(dtrain$y)
+summary(dtrain)
+ 
+ 
+#Validation data 
+dvalid <- h2o.importFile("../1-data/valid_data2.csv")
+dvalid
+class(dvalid)
+summary(dvalid)
+ 
+y <- "y"
+x <- setdiff(names(dvalid), c(y, "id"))
+dvalid$y <- as.factor(dvalid$y)
+summary(dvalid)
+ 
+ 
+ 
+#splits <- h2o.splitFrame(df, c(0.6,0.2), seed=123)
+#train  <- h2o.assign(splits[[1]], "train") # 60%
+#valid  <- h2o.assign(splits[[2]], "valid") # 20%
+#test   <- h2o.assign(splits[[3]], "test")  # 20%
+
+
+model_gbm <- h2o.gbm(x = x,
+                    y = y,
+                    nfolds = 0,
+                    seed = 1000,
+                    training_frame = dtrain,
+                    validation_frame = dvalid,
+                    ntrees = 100,
+                    max_depth = 20,
+                    max_runtime_secs = 200)
+
+perf <- h2o.performance(model_gbm, train = TRUE)
+perf
+perf_valid <- h2o.performance(model_gbm, valid = TRUE)
+perf_valid
+
+#test data 
+dtest <- h2o.importFile("../1-data/test_data2.csv")
+dtest
+class(dtest)
+summary(dtest)
+
+perf_test <- h2o.performance(model_gbm, newdata = dtest)
+perf_test
+
+h2o.auc(perf)
+h2o.auc(perf_valid)
+h2o.auc(perf_test)
+
+
+
+test_data <- h2o.importFile("../1-data/test_data.csv")
+h2o.performance(model_gbm, newdata = test_data)
+
+predictions <- h2o.predict(model_gbm, test_data)
+
+predictions %>%
+  as_tibble() %>%
+  mutate(id = row_number(), y = p0) %>%
+  select(id, y) %>%
+  write_csv("../5-predictions/predictions2.csv")
+
+### ID, Y
+
+h2o.saveModel(model_gbm, filename = "../4-model/my_model2")
+
+
+
+# # # h2o.shutdown()
