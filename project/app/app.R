a06f9fc5397ce179dab6126c690044e054e80fa6
Giemoc <116371400+Giemoc@users.noreply.github.com>
2022-12-21 22:07:40 +0200

    Add files via upload

--- project/app/app.R
+++ project/app/app.R
@@ -1,14 +1,43 @@
+library(shinydashboard)
 library(shiny)
-ui <- fluidPage(
-  titlePanel("Hello world"),
-  sidebarLayout(
-    sidebarPanel(
-    ),
-    mainPanel(
-    )
+library(h2o)
+library(tidyverse)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Loan dashboard 2022"),
+  dashboardSidebar(fileInput("file", "Upload csv file")),
+  dashboardBody(
+    # valueBox(100, "Basic example"),
+    tableOutput("table"),
+    dataTableOutput("predictions")
   )
 )
 server <- function(input, output) {
-
+  h2o.init()
+  model <- h2o.loadModel("../4-model/my_model2")
+  # model <- h2o.import_mojo("GBM_1_AutoML_1_20221124_232404.zip")
+  
+  output$table <- renderTable({
+    req(input$file)
+    table <- read_csv(input$file$datapath) %>%
+      group_by(credit_score) %>%
+      summarise(n = n())
+    table
+  })
+  
+  
+  output$predictions <- renderDataTable({
+    req(input$file)
+    df_test <- h2o.importFile(input$file$datapath)
+    p <- h2o.predict(model, df_test)
+    p %>%
+      as_tibble() %>%
+      mutate(y = predict) %>%
+      select(y) %>%
+      rownames_to_column("id") %>%
+      head(20)
+  })
+  
+  
 }
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
